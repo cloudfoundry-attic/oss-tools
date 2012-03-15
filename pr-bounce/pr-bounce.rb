2f0ecb70db875b8372bb080ee846a4c182b5f7d3
mpage <mpage@vmware.com>
2012-03-15 13:37:54 -0700

    Minor changes to the automatic pull request closer
    Change-Id: I2de6307e29719af116eecdcde4ca4ed301f888d5

--- pr-bounce/pr-bounce.rb
+++ pr-bounce/pr-bounce.rb
@@ -1,40 +1,11 @@
 # Copyright (C) 2012 VMware, Inc. All rights reserved.
-require "rubygems"
-require "octokit"
+require 'bundler'
+Bundler.setup
+
 require "logger"
-require "YAML"
-
-def handle_pull_requests
-  log    = create_logger()
-  log.info("Loading config")
-  config = load_config()
-
-  # trying to use oauth is a tragedy
-  client = Octokit::Client.new(
-    :login    => config["username"],
-    :password => ENV["PR_BOUNCE_PASSWORD"] || config["password"]
-  )
-  log.info("Logged into Github with #{config['username']}")
-  org    = config['organization']
-
-  log.info("Grabbing all public repos for #{org}")
-  repos  = client.organization_repositories(org)
-
-  repos.each do |repo|
-    handle_pull_request(log, config, client, org, repo["name"])
-  end
-end
-
-def create_logger
-  log       = Logger.new(STDOUT)
-  log.level = Logger::INFO
-  return log
-end
-
-def load_config
-  config = YAML::load( File.open( ENV["PR_BOUNCE_CONFIG"] || "config.yaml" ) )
-  return config
-end
+require "octokit"
+require "optparse"
+require "yaml"
 
 def update_pull_request(log, config, client, org, repo, pr)
   closing_template = IO.read(config['templates']['closing'])
@@ -51,17 +22,65 @@ def update_pull_request(log, config, client, org, repo, pr)
   end
 end
 
-def handle_pull_request(log, config, client, org, repo)
+def handle_pull_requests(log, config, client, org, repo)
   log.info("Getting pull requests for #{repo}")
 
-  pulls      = client.pulls(org + "/" + repo)
+  pulls = client.pulls(org + "/" + repo)
   min_pr_num = config['min_pr_num'][repo] || 0
 
   pulls.each do |pr|
-      if pr.number > min_pr_num
-        update_pull_request(log, config, client, org, repo, pr)
-      end
+    if pr.number > min_pr_num
+      update_pull_request(log, config, client, org, repo, pr)
     end
+  end
 end
 
-handle_pull_requests()
+opts = {
+  :verbose => false
+}
+parser = OptionParser.new do |op|
+  op.banner = <<-EOT
+Usage: pr-bounce.rb [options] [/path/to/config.yml]
+
+Automatically close pull requests with a helpful message pointing to the
+CF gerrit instance.
+
+Options:
+EOT
+
+  op.on('-v', '--verbose', 'Display debugging information') do
+    opts[:verbose] = true
+  end
+end
+
+parser.parse!(ARGV)
+
+unless ARGV.length == 1
+  puts parser.help
+  exit 1
+end
+
+begin
+  config = YAML.load_file(ARGV[0])
+  log = Logger.new(STDOUT)
+  log.level = opts[:verbose] ? Logger::DEBUG : Logger::INFO
+
+  # trying to use oauth is a tragedy
+  client = Octokit::Client.new(
+    :login    => config["username"],
+    :password => ENV["PR_BOUNCE_PASSWORD"] || config["password"])
+  log.info("Logged into Github with #{config['username']}")
+  org = config['organization']
+
+  log.info("Grabbing all public repos for #{org}")
+  repos = client.organization_repositories(org)
+
+  repos.each do |repo|
+    handle_pull_requests(log, config, client, org, repo["name"])
+  end
+
+  log.info("Done")
+rescue => e
+  log.error(e.to_s)
+  log.debug(e.backtrace.join("\n"))
+end
