3c3f09384f25a14502683acbf89502e3fcba0127
mpage <mpage@vmware.com>
2012-03-21 19:11:41 -0700

    Skeleton structure for the 'gerrit-cli' gem.
    This will bundle all of the helper functionality currently living
    in gerrit-clone and gerrit-push into a single gem that is available
    on RubyGems.
    
    Change-Id: Ia59718ec0355a2c1ca984e0f8c6f4a81f0ace633

---
+++ gerrit-cli/lib/gerrit/cli/command/base.rb
@@ -0,0 +1,52 @@
+require 'logger'
+require 'optparse'
+
+module Gerrit
+  module Cli
+    module Command
+    end
+  end
+end
+
+class Gerrit::Cli::Command::Base
+
+  attr_reader :option_parser
+
+  def initialize(logger)
+    @logger = logger
+    @option_parser = OptionParser.new
+
+    setup_option_parser
+  end
+
+  def setup_option_parser
+    @option_parser.on('-h', '--help', 'Display usage') do
+      show_usage
+      exit 0
+    end
+
+    @option_parser.on('-v', '--verbose', 'Show debugging information') do
+      @logger.level = Logger::DEBUG
+    end
+  end
+
+  def run(argv)
+    raise NotImplementedError
+  end
+
+  def name
+    self.class.name.split('::').last.downcase
+  end
+
+  def summary
+    @option_parser.banner
+  end
+
+  def usage
+    "Usage: gerrit #{name} [options]\n\n" + @option_parser.help
+  end
+
+  def show_usage
+    @logger.info(usage())
+  end
+end
---
+++ gerrit-cli/lib/gerrit/cli/command/help.rb
@@ -0,0 +1,48 @@
+require 'gerrit/cli/command/base'
+require 'gerrit/cli/errors'
+require 'gerrit/cli/util'
+
+class Gerrit::Cli::Command::Help < Gerrit::Cli::Command::Base
+
+  attr_reader :commands_summary
+
+  def initialize(logger, commands)
+    super(logger)
+
+    @commands = commands.dup.merge(self.name => self)
+
+    rows = @commands.keys.sort.map {|k| [k, @commands[k].summary] }
+    @commands_summary = Gerrit::Cli::Util.render_table(rows)
+  end
+
+  def setup_option_parser
+    super
+
+    @option_parser.banner =
+      "Show a list of commands or display help for a specific command."
+  end
+
+  def usage
+    "Usage: gerrit help [options] [<command>]\n\n" \
+    + @option_parser.help                          \
+    + "\nAvailable commands:\n"                    \
+    + @commands_summary
+  end
+
+  def run(argv)
+    args = @option_parser.parse(argv)
+
+    case args.length
+    when 1
+      if command = @commands[args[0]]
+        command.show_usage
+      else
+        raise Gerrit::Cli::UsageError.new("Unknown command '#{args[0]}'")
+      end
+    when 0
+      show_usage
+    else
+      raise Gerrit::Cli::UsageError.new("Too many arguments")
+    end
+  end
+end
