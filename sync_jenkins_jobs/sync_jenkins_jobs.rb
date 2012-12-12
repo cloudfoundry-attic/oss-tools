2a877e1adfd8c48f70af6b1eceba0f12174a9a81
Leo Li <lileo@rbcon.com>
2012-12-12 02:33:42 +0000

    Add script to sync jenkins role setting
    This is related with http://reviews.cloudfoundry.org/#/c/12321/.
    Running this script could sync the current admins and role setting
    to deployment manifest.
    
    Change-Id: I5f2389d92f999dc8de2be622720654e13abcddbc

--- sync_jenkins_jobs/sync_jenkins_jobs.rb
+++ sync_jenkins_jobs/sync_jenkins_jobs.rb
@@ -3,43 +3,7 @@ require "optparse"
 require "set"
 require "tmpdir"
 require "yaml"
-
-def system!(logger, cmd)
-  logger.info("+ #{cmd}")
-  unless system(cmd)
-    raise "Failed executing '#{cmd}'"
-  end
-
-  true
-end
-
-def parse_jenkins_ip(manifest_path)
-  manifest = YAML.load_file(manifest_path)
-  unless jobs = manifest["jobs"]
-    raise "No jobs found in manifest"
-  end
-
-  jenkins_job = jobs.select {|j| j["name"] == "jenkins"}.first
-  unless jenkins_job
-    raise "Jenkins job not found in manifest"
-  end
-
-  unless networks = jenkins_job["networks"]
-    raise "No networks found for jenkins"
-  end
-
-  nets = Set.new(%w[jenkins default])
-  net = networks.select {|n| nets.include?(n["name"]) }.first
-  unless net
-    raise "No network found"
-  end
-
-  unless ips = net["static_ips"]
-    raise "No static ips found in jenkins default network"
-  end
-
-  ips.first
-end
+require File.expand_path(File.dirname(__FILE__) + '/sync_jenkins_common.rb')
 
 def rsync_live_configs(logger, jenkins_ip)
   job_dir = Dir.mktmpdir
